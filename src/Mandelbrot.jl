03abef5645272017a5165651da39ad613fa4a32a
vasanth <vasanth.mani.vasi.2@gmail.com>
2021-03-02 10:04:28 +0530

    Implement base mandelbrot functions

--- src/Mandelbrot.jl
+++ src/Mandelbrot.jl
@@ -1,5 +1,34 @@
 module Mandelbrot
+export mandelbrot_image, mandelbrot, scale, minmax_scale
 
-# Write your package code here.
+using Base.Threads, Distributed
+
+function mandelbrot(c::Complex, max_iter::Int)
+    z = 0.0 + 0.0im
+    for i in 1:max_iter
+        z = z^2 + c
+        if abs(z) > 2
+            return max_iter - i
+        end
+    end
+    return 0
+end
+
+function mandelbrot_image(width, height, max_iter = 1000; rmin = -2, rmax = 1, imin = -1, imax = 1)
+    @time A = [
+        Complex((j/width * (rmax - rmin) + rmin), (i/height * (imax - imin) + imin))
+        for i in 1:height, j in 1:width
+    ]
+
+    set = Array{Int}(undef, height, width)
+
+    @inbounds @threads for i in 1:height
+        for j in 1:width
+            set[i, j] = mandelbrot(A[i, j], max_iter)
+        end
+    end
+
+    return set
+end
 
 end
